a303edd499713463c536681d80b314b6b56e4fd9
Jessie <playlaugh2011@gmail.com>
2013-10-26 01:37:02 +0800

    init data - user and activity tags

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,6 +1,7 @@
 import play.test.*;
 import play.jobs.*;
 import models.*;
+import models.activity.Activity;
 import models.users.SimpleUser;
 
 @OnApplicationStart
@@ -8,7 +9,10 @@ public class Bootstrap extends Job {
     
     public void doJob() {
         if(SimpleUser.count() == 0) {
-            Fixtures.loadYaml("test.yml");
+            Fixtures.loadModels("init-data-user.yml");
+        }
+        if(Activity.count()==0){
+        	Fixtures.loadModels("init-data-activity.yml");
         }
     }
     
