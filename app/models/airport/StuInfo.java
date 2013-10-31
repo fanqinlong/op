1d84802614fdb0d2023c31ceb8fe95ed9a777b1e
blueappleTony <285899506@qq.com>
2013-10-31 15:09:12 +0800

    chenge pick up

--- app/models/airport/StuInfo.java
+++ app/models/airport/StuInfo.java
@@ -2,6 +2,7 @@ package models.airport;
 
 import javax.persistence.Entity;
 
+import models.qa.Ques;
 import play.data.validation.Required;
 import play.db.jpa.Model;
 import play.libs.Codec;
@@ -29,5 +30,4 @@ public class StuInfo extends Model {
 	@Required
 	public String luggage;
 	public String remarks;
-
 }
