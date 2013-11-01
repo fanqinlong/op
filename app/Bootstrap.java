f8d71dc3eb0d1260545e3f706702ea95b5d40da8
playlaugh <playlaugh2011@gmail.com>
2013-11-01 21:27:33 +0800

    fixed zhang

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -7,20 +7,13 @@ import models.users.SimpleUser;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
-    
-    public void doJob() {
-        if(SimpleUser.count() == 0) {
-            Fixtures.loadModels("init-data-user.yml");
-            Fixtures.loadModels("init-data-activity.yml");
-        }
-        
-        
-        if(Wel.count()==0){
-        	
-        	Fixtures.loadModels("init-data-charity.yml");
-        	
-        }
-       
-    }
-    
+
+	public void doJob() {
+		if (SimpleUser.count() == 0) {
+			Fixtures.loadModels("init-data-user.yml");
+			Fixtures.loadModels("init-data-activity.yml");
+		}
+
+	}
+
 }
