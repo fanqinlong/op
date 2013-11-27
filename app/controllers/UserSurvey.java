b5a2297e529277a26fbc484bf3fa5bb90b76918f
ZSKZ <710162063@qq.com>
2013-11-27 18:21:04 +0800

    lastupdate

--- app/controllers/UserSurvey.java
+++ app/controllers/UserSurvey.java
@@ -6,6 +6,7 @@ import play.mvc.*;
 public class UserSurvey extends Controller {
 	public static void index(String name, String type, String content,
 			String contact) {
+		System.out.println("看看选择了是呢么"+type);
 		new Questioner(name, type, content, contact);
 		
 		System.out.println("1111");
