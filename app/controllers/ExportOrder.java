10e9ff36332250fc037f6a5a8e9d89d6f599afeb
ZSKZ <710162063@qq.com>
2014-01-22 18:51:40 +0800

    pagination

--- app/controllers/ExportOrder.java
+++ app/controllers/ExportOrder.java
@@ -7,7 +7,6 @@ import play.modules.excel.RenderExcel;
 import models.Meal.MealOrder;
 import models.airport.StuInfo;
 import models.airport.VolInfo;
-import models.users.SimpleUser;
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -18,30 +17,14 @@ public class ExportOrder extends Controller {
 		List<MealOrder> mealOrders = MealOrder.findAll();
 		render(mealOrders);
 	}
-
-	public static void exportOrder(String shopName) {
-		long userid = Long.parseLong(session.get("logged"));
-		SimpleUser simpleUser = SimpleUser.findById(userid);
-		System.out.println("看看店面1"+shopName);
-		System.out.println("看看店面2"+simpleUser.shopName);
-		if (shopName.equals(simpleUser.shopName)) {
-			request.format = "xlsx";
-			List<MealOrder> voi = MealOrder.find(
-					"SELECT a FROM MealOrder a WHERE shopName LIKE ?", shopName)
-					.fetch();
-			String __FILE_NAME__ = shopName+"_订单.xlsx";
-			renderArgs.put(RenderExcel.RA_ASYNC, true);
-			render(__FILE_NAME__, voi);
-		} else {
-			flash.error("错误:没有你要查询的信息!");
-			lookOr(userid);
-		}
-
-	}
-
-	private static void lookOr(Long user_id) {
-		// TODO Auto-generated method stub
-
+	public static void exportOrder() {
+		request.format = "xlsx";
+		List<MealOrder> voi = MealOrder.find(
+				"SELECT a FROM MealOrder a WHERE shop_name LIKE ?",
+				"%" + "test" + "%").fetch();
+		String __FILE_NAME__ = "test" + "_订单.xlsx";
+		renderArgs.put(RenderExcel.RA_ASYNC, true);
+		render(__FILE_NAME__, voi);
 	}
 
 }
